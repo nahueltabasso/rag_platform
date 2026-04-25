f6de349fb3c9f8ff106586620bfd8465dc062313
Nahuel Tabasso <nahueltabasso@gmail.com>
2026-04-25 09:31:37 -0300

    docs: update of readme

--- rag/src/test_app.py
+++ rag/src/test_app.py
@@ -1,5 +1,3 @@
-import json
-from rag.src.config_schema import RAGConfig
 from rag_system import get_rag_service
 
 CONFIG_PATH = "rag/config/wwII_wiki_config.json"
@@ -23,5 +21,18 @@ while True:
     
     response, _ = rag_service.process_query(query=user_input, session_id=session_id)
     print(f"Assistant: {response}\n\n") # type: ignore
+    
+    
+    
+# USER INPUT --> QUERY
+# CARGA DEL HISTORIAL DE LA SESION
+# REFORMULACION DE LA PREGUNTA
+# GENERACION DE PREGUNTAS EN BASE A LA PREGUNTA REFORMULADA
+# OBTENCION DE DOCUMENTOS RELEVANTES
+# FILTRADO DE DOCUMENTOS RELEVANTES EN BASE A LA PREGUNTA REFORMULADA
+# CONSTRUIR CONTEXTO
+# GENERACION DE RESPUESTA EN BASE A LOS DOCUMENTOS RELEVANTES
+# ACTUALIZACION DEL HISTORIAL DE LA SESION
+# RESPUESTA AL USUARIO
     
         
